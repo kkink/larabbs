097e2d60be509fb7449ece3e0f7a60ee1b4ae0b4
kk <kk@home.com>
2022-03-14 05:32:54 +0800

    短信调试

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,21 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::prefix('v1')->name('api.v1.')->group(function() {
-
+    Route::get('smstest',function () {
+        $sms = app('easysms');
+        try {
+            $sms->send(13929410175, [
+                'content'  => '您正在使用阿里云短信测试服务，体验验证码是：${code}，如非本人操作，请忽略本短信！',
+                'template' => 'SMS_154950909',
+                'data' => [
+                    'code' => 1234
+                ],
+            ]);
+        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+            $message = $exception->getException('aliyun')->getMessage();
+            dd($message);
+        }
+    });
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
